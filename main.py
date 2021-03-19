e35c63373829f1b0b4b9a1d030dcdaf972203fab
Natchanon Uthitasan <kazuokanarita9@gmail.com>
2021-03-19 19:43:25 +0700

    Add file main
    Day 1 file

---
+++ main.py
@@ -0,0 +1,50 @@
+# print("Hello World!\nHello World!\nHello World!")
+# print("Hello" + "kazuoka")
+# print("Hello" + " " + "kazuoka")
+# print("What is your name?")
+# input("What is your name?") 
+# print("Hello " + input("What is your name?") + "!")
+# print( len( input("What is your name? ") ) )
+
+# name = "Jack"
+# print(name)
+
+# name = "Kazuoka"
+# print(name)
+
+# name = input("What is your name?")
+# length = len(name)
+# print(length) 
+
+#Write your code below this line 👇
+
+#print(Day 1 - String Manipulation")
+#print("String Concatenation is done with the "+" sign.")
+#  print('e.g. print("Hello " + "world")')
+#print(("New lines can be created with a backslash and n.") 
+
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# input("What is your name? ")
+# print( len( input("What is your name? ") ) )
+
+#1. Create a greeting for your program.
+print("Welcome to the band name generator.")
+#2. Ask the user for the city that they grew up in.
+city = input("Which city did you grow up in?\n") 
+#3. Ask the user for the name of a pet.  
+pet = input("What is the name of a pet?\n")
+#4. Combine the name of their city and pet and show them their band name.  
+print("Your band name could be " + city + " " + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+# https://band-name-generator-end.appbrewery.repl.run/
+
+
+
+
+
+
+
