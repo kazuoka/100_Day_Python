b612998fe44431a66e060c5ffb85702cf90c6718
Natchanon Uthitasan <kazuokanarita9@gmail.com>
2021-04-02 13:09:26 +0700

    Add file day 10
    file day 10 , art.py , pyproject.toml , poetry.lock

---
+++ day_10.py
@@ -0,0 +1,190 @@
+# https://stackoverflow.com/questions/8347048/how-to-convert-string-to-title-case-in-python
+
+
+#Functions with Outputs
+
+def format_name(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  return f"Result: {formated_f_name} {formated_l_name}"
+
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
+  
+
+
+
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+def days_in_month(year, month):
+  # if month > 12 or month < 1:
+  #   return "Invalid month"
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if is_leap(year) and month == 2:
+    return 29
+  return month_days[month - 1]  
+  
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+
+
+
+
+#Functions with Outputs
+def format_name(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  f"Result: {formated_f_name} {formated_l_name}"
+
+#Storing output in a variable
+formatted_name = format_name(input("Your first name: "), input("Your last name: "))
+print(formatted_name)
+#or printing output directly
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
+
+#Already used functions with outputs.
+length = len(formatted_name)
+
+#Return as an early exit
+def format_name(f_name, l_name):
+  """Take a first and last name and format it 
+  to return the title case version of the name."""
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  return f"Result: {formated_f_name} {formated_l_name}"
+
+
+
+
+
+#Calculator
+from art import logo
+
+#Add
+def add(n1, n2):
+  return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+  return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+  return n1 * n2
+
+#Divide
+def divide(n1, n2):
+  return n1 / n2
+
+  operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide
+  }
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+      num1 = answer
+    else :
+      should_continue = False
+      calculator()
+
+calculator()
+
+  
+
+
+
+
+from replit import clear
+from art import logo
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      clear()
+      calculator()
+
+calculator()
+
+
+
+
+
+
+
+
+
+
+
